935d3cb5bdb0071fe27d7aab9c50f5d4b4aa6ac6
Emerson Ferreira <emersonbferreira157@gmail.com>
2026-03-13 14:43:49 -0300

    Exercício de números fatoriais do HackerRank 'Extra Long Fatorials'

---
+++ extraLongFatorials.js
@@ -0,0 +1,10 @@
+function extraLongFactorials(n) {
+    n = BigInt(n);
+    let num = BigInt(1);
+    for(let index = n; index > 1; index --){
+        num *= index;
+    }
+    console.log(num.toString());
+}
+
+extraLongFactorials(25);
